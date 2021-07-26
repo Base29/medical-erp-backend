f451444804c54d694d4b4f7246770de89144d60c
Faisal Hussain <faisal@base29.com>
2021-07-26 11:59:20 +0500

    created a migration for signatures table

---
+++ database/migrations/2021_07_26_064131_create_signatures_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSignaturesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('signatures', function (Blueprint $table) {
+            $table->id();
+            $table->text('comment');
+            $table->boolean('comment_visible')->default(false);
+            $table->boolean('confirmation')->default(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('signatures');
+    }
+}
