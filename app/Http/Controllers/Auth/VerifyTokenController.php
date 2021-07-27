ce5edb0b79ec760c8c106ba8000036337106ad80
Faisal Hussain <faisal@base29.com>
2021-07-28 00:49:58 +0500

    added information regarding user roles and practices to verify_token method of VerifyTokenController

--- app/Http/Controllers/Auth/VerifyTokenController.php
+++ app/Http/Controllers/Auth/VerifyTokenController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Arr;
 use Tymon\JWTAuth\Facades\JWTAuth;
 
 class VerifyTokenController extends Controller
@@ -10,9 +11,26 @@ class VerifyTokenController extends Controller
 
     public function verify_token()
     {
+
+        $user = JWTAuth::parseToken()->authenticate();
+        $user_roles = $user->roles->makeHidden([
+            'pivot',
+            'created_at',
+            'updated_at',
+            'guard_name',
+        ]);
+
+        $user_practices = $user->practices->makeHidden([
+            'pivot',
+            'created_at',
+            'updated_at',
+            'deleted_at',
+        ]);
+        $userArr = Arr::add($user, 'roles', $user_roles);
+        $userWithPractices = Arr::add($userArr, 'practices', $user_practices);
         return response([
             'success' => true,
-            'user' => JWTAuth::parseToken()->authenticate(),
+            'user' => $userWithPractices,
         ], 200);
     }
 }
