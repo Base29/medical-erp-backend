69f84e59dcd458e4c2766f62dc12b68be1eb59ef
Faisal Hussain <faisal@base29.com>
2021-07-28 00:55:46 +0500

    renamed variables in verify_token method of VerifyTokenController

--- app/Http/Controllers/Auth/VerifyTokenController.php
+++ app/Http/Controllers/Auth/VerifyTokenController.php
@@ -13,24 +13,26 @@ class VerifyTokenController extends Controller
     {
 
         $user = JWTAuth::parseToken()->authenticate();
-        $user_roles = $user->roles->makeHidden([
-            'pivot',
-            'created_at',
-            'updated_at',
-            'guard_name',
-        ]);
+        $user_roles = $user->roles
+            ->makeHidden([
+                'pivot',
+                'created_at',
+                'updated_at',
+                'guard_name',
+            ]);
 
-        $user_practices = $user->practices->makeHidden([
-            'pivot',
-            'created_at',
-            'updated_at',
-            'deleted_at',
-        ]);
-        $userArr = Arr::add($user, 'roles', $user_roles);
-        $userWithPractices = Arr::add($userArr, 'practices', $user_practices);
+        $user_practices = $user->practices
+            ->makeHidden([
+                'pivot',
+                'created_at',
+                'updated_at',
+                'deleted_at',
+            ]);
+        $user_with_roles = Arr::add($user, 'roles', $user_roles);
+        $user_with_practices = Arr::add($user_with_roles, 'practices', $user_practices);
         return response([
             'success' => true,
-            'user' => $userWithPractices,
+            'user' => $user_with_practices,
         ], 200);
     }
 }
