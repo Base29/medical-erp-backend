c0d2c8f0459feee15b54886061fade3c6c6ae96c
Faisal Hussain <faisal@base29.com>
2021-08-04 19:30:34 +0500

    refactored validation in PasswordResetLinkController

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -9,7 +9,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 
 class PasswordResetLinkController extends Controller
 {
@@ -17,20 +19,41 @@ class PasswordResetLinkController extends Controller
     {
         $credentials = request()->validate(['email' => 'required|email']);
 
+        // Validation rules
+        $rules = [
+            'email' => 'required|email',
+        ];
+
+        // Validating params in request
+        $validator = Validator::make($request->all(), $rules);
+
+        // If validation fails
+        if ($validator->fails()) {
+            $errors = $validator->errors();
+
+            // Return error messages for email
+            if (Arr::has($errors->messages(), 'email')) {
+                return response([
+                    'success' => false,
+                    'message' => $errors->messages()['email'][0],
+                ], 422);
+            }
+        }
+
         $user = User::where('email', $request->only('email'))->first();
 
         if (!$user) {
             return response([
                 'success' => false,
-                'message' => 'Invalid user',
-            ]);
+                'message' => 'User not found',
+            ], 404);
         }
 
-        Password::sendResetLink($credentials);
+        Password::sendResetLink($request->email);
 
         return response([
             'success' => true,
             'message' => 'Reset password link sent on your email id.',
-        ]);
+        ], 200);
     }
 }
