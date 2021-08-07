59dc68547ca3c1b6ab8f520d4c0877fea351d175
Faisal Hussain <faisal@base29.com>
2021-08-07 14:18:34 +0500

    removed debugging statements

--- app/Helpers/CustomValidation.php
+++ app/Helpers/CustomValidation.php
@@ -13,7 +13,6 @@ class CustomValidation
     public static function error_messages($rules, $validator)
     {
         foreach ($rules as $key => $value) {
-            ray($key);
             $errors = $validator->errors();
 
             // Return error messages for email
