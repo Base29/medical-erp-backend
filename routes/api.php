5d2929f02d507670e55ec8ebf6568045359fb102
Faisal Hussain <faisal@base29.com>
2021-08-10 14:18:08 +0500

    added endpoint for fetching rooms for practices

--- routes/api.php
+++ routes/api.php
@@ -25,6 +25,7 @@ use App\Http\Controllers\Role\ListRolesController;
 use App\Http\Controllers\Role\RevokeRoleController;
 use App\Http\Controllers\Room\CreateRoomController;
 use App\Http\Controllers\Room\DeleteRoomController;
+use App\Http\Controllers\Room\ListRoomsController;
 use App\Http\Controllers\Signature\SignPolicyController;
 use App\Http\Controllers\User\CreateUserController;
 use App\Http\Controllers\User\DeleteUserController;
@@ -103,6 +104,7 @@ Route::middleware(['auth:api'])->group(function () {
 
         // Endpoints for room operations
         Route::prefix('rooms')->group(function () {
+            Route::get('/', ListRoomsController::class);
             Route::post('create', CreateRoomController::class);
             Route::delete('delete/{id}', DeleteRoomController::class);
         });
