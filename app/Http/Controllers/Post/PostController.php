0b1a40387f373cd8e6a027c8ee54a1b7fdfc5bcf
Faisal Hussain <faisal@base29.com>
2021-09-28 20:50:42 +0500

    added functionality to create communication-book post

--- app/Http/Controllers/Post/PostController.php
+++ app/Http/Controllers/Post/PostController.php
@@ -2,8 +2,14 @@
 
 namespace App\Http\Controllers\Post;
 
+use App\Helpers\CustomValidation;
+use App\Helpers\FileUpload;
 use App\Http\Controllers\Controller;
+use App\Models\Post;
+use App\Models\PostAttachment;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 
 class PostController extends Controller
 {
@@ -11,9 +17,12 @@ class PostController extends Controller
     {
         // Validation rules
         $rules = [
-            'name' => 'required',
-            'practice' => 'required|numeric',
-            'icon' => 'required',
+            'title' => 'required|string',
+            'subject' => 'required|string',
+            'message' => 'required|string',
+            'category' => 'required|string',
+            'type' => 'required|string',
+            'attachment' => 'file|mimes:doc,docx,pdf,jpg,png,jpeg|nullable',
         ];
 
         // Validation errors
@@ -23,5 +32,32 @@ class PostController extends Controller
         if ($request_errors) {
             return $request_errors;
         }
+
+        // Create Post
+        $post = new Post();
+        $post->title = $request->title;
+        $post->subject = $request->subject;
+        $post->message = $request->message;
+        $post->category = $request->category;
+        $post->type = $request->type;
+        $post->user_id = auth()->user()->id;
+        $post->save();
+
+        // If file is attached when creating a post
+        if ($request->has('attachment')) {
+            $attachment_url = FileUpload::upload(request()->attachment, 'communication-book', 's3');
+            $attachment = new PostAttachment();
+            $attachment->url = $attachment_url;
+            $post->post_attachments()->save($attachment);
+        }
+
+        // Adding attachments to the response
+        Arr::add($post, 'attachments', $post->post_attachments()->get());
+
+        return response([
+            'success' => true,
+            'post' => $post,
+        ], 200);
+
     }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use App\Models\Answer;
 use App\Models\Comment;
+use App\Models\PostAttachment;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -19,6 +20,7 @@ class Post extends Model
         'message',
         'category',
         'type',
+        'user_id',
     ];
 
     public function answers()
@@ -34,5 +36,10 @@ class Post extends Model
     public function user()
     {
         return $this->belongsTo(User::class);
+    }
+
+    public function post_attachments()
+    {
+        return $this->hasMany(PostAttachment::class, 'post_id', 'id');
     }
 }
