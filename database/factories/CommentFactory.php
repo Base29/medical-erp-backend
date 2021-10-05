5779e530332c6c95657c27a0d8e23b282c78c533
Faisal Hussain <faisal@base29.com>
2021-10-05 14:19:10 +0500

    added seeder and factory for post comments

---
+++ database/factories/CommentFactory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Comment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class CommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Comment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'post_id' => 73,
+            'user_id' => 9,
+            'comment' => $this->faker->paragraph(3, true),
+        ];
+    }
+}
