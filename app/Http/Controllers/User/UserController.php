225c436b88aac3e1c27950cc14150802be0e0e05
Faisal Hussain <faisal@base29.com>
2021-10-14 17:12:10 +0500

    replaced the response with the custom Response Class in app/Http/Controllers/User/UserController.php

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use App\Helpers\CustomValidation;
+use App\Helpers\Response;
 use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -15,7 +16,7 @@ class UserController extends Controller
     {
         // Validation rules
         $rules = [
-            'email' => 'required|email',
+            'email' => 'required|email|unique:users,email',
             'name' => 'required',
             'password' => 'required|confirmed',
         ];
@@ -28,15 +29,6 @@ class UserController extends Controller
             return $request_errors;
         }
 
-        // Check if the user already exists
-        $user_exists = User::where('email', $request->email)->first();
-        if ($user_exists) {
-            return response([
-                'success' => false,
-                'message' => 'User already exists with email ' . $request->email,
-            ], 409);
-        }
-
         // Create user
         $user = new User();
         $user->email = $request->email;
@@ -44,16 +36,7 @@ class UserController extends Controller
         $user->password = Hash::make($request->password);
         $user->save();
 
-        return $user ?
-        response([
-            'success' => true,
-            'message' => 'New user created with email ' . $user->email,
-        ], 200)
-        :
-        response([
-            'success' => false,
-            'message' => 'Something went wrong while creating user',
-        ], 400);
+        return Response::success(['user' => $user]);
     }
 
     // Method for deleting user
@@ -63,29 +46,24 @@ class UserController extends Controller
         $user = User::find($id);
 
         if (!$user) {
-            return response([
-                'success' => false,
-                'message' => 'No user found with the provided id ' . $id,
-            ], 404);
+            return Response::fail([
+                'message' => 'User with ID ' . $id . ' not found',
+                'code' => 404,
+            ]);
         }
 
         // Delete user with the provided $id
         $user->delete();
-        return response([
-            'success' => true,
-            'message' => 'User delete successfully',
-        ], 200);
+
+        return Response::success(['message' => 'User delete successfully']);
     }
 
     // Method for fetching users
     public function fetch()
     {
         // Fetching all the users from database
-        $users = User::with('roles', 'practices')->paginate(10);
+        $users = User::with('roles', 'practices')->latest()->paginate(10);
 
-        return response([
-            'success' => true,
-            'users' => $users,
-        ], 200);
+        return Response::success(['users' => $users]);
     }
 }
