c1161f5980035b43c41bb06963fba17c3c6034f2
Faisal Hussain <faisal@base29.com>
2021-10-26 21:13:35 +0500

    modification to app/Http/Controllers/Signature/SignPolicyController.php

--- app/Http/Controllers/Signature/SignPolicyController.php
+++ app/Http/Controllers/Signature/SignPolicyController.php
@@ -12,30 +12,40 @@ class SignPolicyController extends Controller
 {
     public function __invoke(Request $request)
     {
-        // Fetching policy and related signatures
-        $policy = Policy::where('id', $request->policy_id)->with('signatures')->first();
+        try {
 
-        // Checking if the current logged in user has already signed the policy
-        $already_signed = $policy->signatures->contains('user_id', auth()->user()->id);
+            // Fetching policy and related signatures
+            $policy = Policy::where('id', $request->policy_id)->with('signatures')->firstOrFail();
 
-        //TODO: Commented the below code block for testing purpose. This should be un-commented in production
-        // Returning response incase the policy is already signed by the current logged in user
-        // if ($already_signed) {
-        //     return response([
-        //         'success' => false,
-        //         'message' => auth()->user()->name . ' has already signed ' . $policy->name,
-        //     ]);
-        // }
+            // Checking if the current logged in user has already signed the policy
+            $already_signed = $policy->signatures->contains('user_id', auth()->user()->id);
 
-        // Creating signature of the current logged in user
-        $signature = new Signature();
-        $signature->comment = $request->comment;
-        $signature->confirmation = $request->confirmation;
-        $signature->user_id = auth()->user()->id;
-        $signature->policy_id = $request->policy_id;
-        $signature->save();
+            //TODO: Commented the below code block for testing purpose. This should be un-commented in production
+            // Returning response incase the policy is already signed by the current logged in user
+            // if ($already_signed) {
+            //     return response([
+            //         'success' => false,
+            //         'message' => auth()->user()->name . ' has already signed ' . $policy->name,
+            //     ]);
+            // }
 
-        // Returning response if the policy is successfully sgined by the currently logged in user
-        return Response::success(['signature' => $signature->with('policy')->latest()->first()]);
+            // Creating signature of the current logged in user
+            $signature = new Signature();
+            $signature->comment = $request->comment;
+            $signature->confirmation = $request->confirmation;
+            $signature->user_id = auth()->user()->id;
+            $signature->policy_id = $request->policy_id;
+            $signature->save();
+
+            // Returning response if the policy is successfully sgined by the currently logged in user
+            return Response::success(['signature' => $signature->with('policy')->latest()->firstOrFail()]);
+
+        } catch (\Exception $e) {
+
+            return Response::fail([
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 }
