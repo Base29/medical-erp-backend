5f36eb9935ab9f2ba1f74c70b421e785997557c5
Faisal Hussain <faisal@base29.com>
2021-10-26 20:51:05 +0500

    modification to app/Http/Controllers/Practice/PracticeController.php

--- app/Http/Controllers/Practice/PracticeController.php
+++ app/Http/Controllers/Practice/PracticeController.php
@@ -17,67 +17,108 @@ class PracticeController extends Controller
     public function create(CreatePracticeRequest $request)
     {
 
-        // Create practice with the provided name
-        $practice = Practice::create([
-            'practice_name' => $request->name,
-        ]);
+        try {
 
-        return Response::success(['practice' => $practice]);
+            // Create practice with the provided name
+            $practice = new Practice();
+            $practice->practice_name = $request->name;
+            $practice->save();
+
+            return Response::success(['practice' => $practice]);
+
+        } catch (\Exception $e) {
+
+            return Response::fail([
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ]);
+        }
+
     }
 
     // Method for deleting practice
     public function delete($id)
     {
-        // Check if practice exists
-        $practice = Practice::find($id);
+        try {
 
-        if (!$practice) {
+            // Check if practice exists
+            $practice = Practice::findOrFail($id);
+
+            if (!$practice) {
+                return Response::fail([
+                    'message' => ResponseMessage::notFound('Practice', $id, false),
+                    'code' => 404,
+                ]);
+            }
+
+            // Deleting practice
+            $practice->delete();
+
+            return Response::success(['message' => ResponseMessage::deleteSuccess('Practice')]);
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::notFound('Practice', $id, false),
-                'code' => 404,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Deleting practice
-        $practice->delete();
-
-        return Response::success(['message' => ResponseMessage::deleteSuccess('Practice')]);
 
     }
 
     // Method for fetching practices
     public function fetch()
     {
-        // Fetch practices
-        $practices = Practice::with('policies')->paginate(10);
+        try {
 
-        return Response::success(['practices' => $practices]);
+            // Fetch practices
+            $practices = Practice::with('policies')->paginate(10);
+
+            return Response::success(['practices' => $practices]);
+
+        } catch (\Exception $e) {
+
+            return Response::fail([
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ]);
+        }
     }
 
     // Method for assigning user to practice
     public function assign_to_user(AssignPracticeToUserRequest $request)
     {
 
-        // Get User
-        $user = User::where('email', $request->email)->first();
+        try {
 
-        // Get Practice
-        $practice = Practice::find($request->practice);
+            // Get User
+            $user = User::where('email', $request->email)->firstOrFail();
 
-        // Checking if the user is already assigned to the provided practice
-        $user_already_assigned_to_practice = $user->practices->contains('id', $practice->id);
+            // Get Practice
+            $practice = Practice::findOrFail($request->practice);
 
-        if ($user_already_assigned_to_practice) {
+            // Checking if the user is already assigned to the provided practice
+            $user_already_assigned_to_practice = $user->practices->contains('id', $practice->id);
+
+            if ($user_already_assigned_to_practice) {
+                return Response::fail([
+                    'message' => ResponseMessage::alreadyAssigned($user->email, $practice->practice_name),
+                    'code' => 409,
+                ]);
+            }
+
+            // Attach user to practice
+            $user->practices()->attach($practice->id);
+
+            return Response::success(['message' => ResponseMessage::assigned($user->email, $practice->practice_name)]);
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::alreadyAssigned($user->email, $practice->practice_name),
-                'code' => 409,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Attach user to practice
-        $user->practices()->attach($practice->id);
-
-        return Response::success(['message' => ResponseMessage::assigned($user->email, $practice->practice_name)]);
 
     }
 
@@ -85,25 +126,35 @@ class PracticeController extends Controller
     public function revoke_for_user(RevokePracticeForUserRequest $request)
     {
 
-        // Get User
-        $user = User::where('email', $request->email)->first();
+        try {
 
-        // Get Practice
-        $practice = Practice::find($request->practice);
+            // Get User
+            $user = User::where('email', $request->email)->firstOrFail();
 
-        // Check if the user is already assigned to the practice
-        $associated_to_practice = $user->practices->contains('id', $practice->id);
+            // Get Practice
+            $practice = Practice::findOrFail($request->practice);
 
-        if (!$associated_to_practice) {
+            // Check if the user is already assigned to the practice
+            $associated_to_practice = $user->practices->contains('id', $practice->id);
+
+            if (!$associated_to_practice) {
+                return Response::fail([
+                    'message' => ResponseMessage::notBelongTo($user->email, $practice->practice_name),
+                    'code' => 409,
+                ]);
+            }
+
+            // Revoke user from practice
+            $user->practices()->detach($practice->id);
+
+            return Response::success(['message' => ResponseMessage::revoked($user->email, $practice->practice_name)]);
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::notBelongTo($user->email, $practice->practice_name),
-                'code' => 409,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Revoke user from practice
-        $user->practices()->detach($practice->id);
-
-        return Response::success(['message' => ResponseMessage::revoked($user->email, $practice->practice_name)]);
     }
 }
