1c7e82a9ce7b89606d225690540359073d8b95de
Faisal Hussain <faisal@base29.com>
2021-10-26 21:16:32 +0500

    modification to app/Http/Controllers/Task/TaskController.php

--- app/Http/Controllers/Task/TaskController.php
+++ app/Http/Controllers/Task/TaskController.php
@@ -15,77 +15,108 @@ class TaskController extends Controller
     // Method for creating a task
     public function create(CreateTaskRequest $request)
     {
-        // Check if the checklist exists
-        $checklist = CheckList::where('id', $request->checklist)->with('tasks')->first();
+        try {
 
-        // Check if the task with same name already exists in the checklist
-        $task_already_exist = $checklist->tasks->contains('name', $request->name);
+            // Check if the checklist exists
+            $checklist = CheckList::where('id', $request->checklist)->with('tasks')->firstOrFail();
 
-        if ($task_already_exist) {
+            // Check if the task with same name already exists in the checklist
+            $task_already_exist = $checklist->tasks->contains('name', $request->name);
+
+            if ($task_already_exist) {
+                return Response::fail([
+                    'message' => ResponseMessage::alreadyExists($request->name),
+                    'code' => 409,
+                ]);
+            }
+
+            // Create task
+            $task = new Task();
+            $task->name = $request->name;
+            $task->check_list_id = $checklist->id;
+            $task->frequency = $request->frequency;
+            $task->save();
+
+            return Response::success(['task' => $task]);
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::alreadyExists($request->name),
-                'code' => 409,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Create task
-        $task = new Task();
-        $task->name = $request->name;
-        $task->check_list_id = $checklist->id;
-        $task->frequency = $request->frequency;
-        $task->save();
-
-        return Response::success(['task' => $task]);
     }
 
     // Method for deleting a task
     public function delete($id)
     {
-        // Check if a task exists with a provided $id
-        $task = Task::find($id);
 
-        if (!$task) {
+        try {
+
+            // Check if a task exists with a provided $id
+            $task = Task::findOrFail($id);
+
+            if (!$task) {
+                return Response::fail([
+                    'message' => ResponseMessage::notFound('Task', $id, false),
+                    'code' => 404,
+                ]);
+            }
+
+            // Delete task
+            $task->delete();
+
+            return Response::success(['message' => ResponseMessage::deleteSuccess('Task')]);
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::notFound('Task', $id, false),
-                'code' => 404,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Delete task
-        $task->delete();
-
-        return Response::success(['message' => ResponseMessage::deleteSuccess('Task')]);
     }
 
     public function update(Request $request)
     {
-        // Allowed fields when updating a task
-        $allowed_fields = [
-            'status',
-            'reason',
-            'comment',
-            'manager_comment',
-            'acknowledgement',
-        ];
 
-        // Checking if the $request doesn't contain any of the allowed fields
-        if (!$request->hasAny($allowed_fields)) {
+        try {
+
+            // Allowed fields when updating a task
+            $allowed_fields = [
+                'status',
+                'reason',
+                'comment',
+                'manager_comment',
+                'acknowledgement',
+            ];
+
+            // Checking if the $request doesn't contain any of the allowed fields
+            if (!$request->hasAny($allowed_fields)) {
+                return Response::fail([
+                    'message' => ResponseMessage::allowedFields($allowed_fields),
+                    'code' => 400,
+                ]);
+            }
+
+            // Get Task
+            $task = Task::findOrFail($request->task);
+
+            // Update task's fields with the ones provided in the $request
+            $task_updated = $this->update_task($request->all(), $task);
+
+            if ($task_updated) {
+                return Response::success(['task' => $task]);
+            }
+
+        } catch (\Exception $e) {
+
             return Response::fail([
-                'message' => ResponseMessage::allowedFields($allowed_fields),
-                'code' => 400,
+                'code' => 500,
+                'message' => $e->getMessage(),
             ]);
         }
-
-        // Get Task
-        $task = Task::find($request->task);
-
-        // Update task's fields with the ones provided in the $request
-        $task_updated = $this->update_task($request->all(), $task);
-
-        if ($task_updated) {
-            return Response::success(['task' => $task]);
-        }
-
     }
 
     private function update_task($fields, $task)
