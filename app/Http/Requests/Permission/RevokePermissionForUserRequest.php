639c5fc916f9de8b9adf1673b6a35d0443731169
Faisal Hussain <faisal@base29.com>
2021-10-29 18:28:44 +0500

    bug fixes in request layers

--- app/Http/Requests/Permission/RevokePermissionForUserRequest.php
+++ app/Http/Requests/Permission/RevokePermissionForUserRequest.php
@@ -15,7 +15,7 @@ class RevokePermissionForUserRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
--- app/Http/Requests/Room/UpdateRoomRequest.php
+++ app/Http/Requests/Room/UpdateRoomRequest.php
@@ -15,7 +15,7 @@ class UpdateRoomRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
