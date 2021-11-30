15befc93554389a99b834c378b1749f2d1696421
Faisal Hussain <faisal@base29.com>
2021-11-30 16:29:08 +0500

    added request rules in app/Http/Requests/MiscellaneousInformation/CreateMiscellaneousInformationRequest.php

--- app/Models/MiscellaneousInformation.php
+++ app/Models/MiscellaneousInformation.php
@@ -11,6 +11,22 @@ class MiscellaneousInformation extends Model
 {
     use HasFactory, SoftDeletes;
 
+    protected $fillable = [
+        'job_description',
+        'interview_notes',
+        'offer_letter_email',
+        'job_advertisement',
+        'health_questionnaire',
+        'annual_declaration',
+        'employee_confidentiality_agreement',
+        'employee_privacy_notice',
+        'locker_key_agreement',
+        'is_locker_key_assigned',
+        'equipment_provided_policy',
+        'resume',
+        'proof_of_address',
+    ];
+
     public function user()
     {
         return $this->belongsTo(User::class);
