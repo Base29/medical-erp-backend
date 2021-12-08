7530927bcddb937a2f17d417ad4dcb1504ca6969
Faisal Hussain <faisal@base29.com>
2021-12-08 16:54:14 +0500

    created app/Http/Controllers/EmploymentPolicy/EmploymentPolicyController.php

---
+++ app/Http/Controllers/EmploymentPolicy/EmploymentPolicyController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers\EmploymentPolicy;
+
+use App\Http\Controllers\Controller;
+
+class EmploymentPolicyController extends Controller
+{
+    // Create Employment Policy
+    public function create()
+    {
+        //
+    }
+}
