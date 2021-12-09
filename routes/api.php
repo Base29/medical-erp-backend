a67fb4cd066fd373b8877ea6e763a15ea6c16269
Faisal Hussain <faisal@base29.com>
2021-12-09 16:37:30 +0500

    added api route for creating employment history

--- routes/api.php
+++ routes/api.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\CheckList\CheckListController;
 use App\Http\Controllers\Comment\CommentController;
 use App\Http\Controllers\ContractSummary\ContractSummaryController;
 use App\Http\Controllers\EmploymentCheck\EmploymentCheckController;
+use App\Http\Controllers\EmploymentHistory\EmploymentHistoryController;
 use App\Http\Controllers\EmploymentPolicy\EmploymentPolicyController;
 use App\Http\Controllers\MiscellaneousInformation\MiscellaneousInformationController;
 use App\Http\Controllers\Permission\PermissionController;
@@ -335,5 +336,11 @@ Route::middleware(['auth:api'])->group(function () {
 
         Route::post('/', [EmploymentPolicyController::class, 'fetch'])
             ->middleware(['permission:can_fetch_employment_policies']);
+    });
+
+    // Routes for employment history
+    Route::prefix('employment-histories')->group(function () {
+        Route::post('create', [EmploymentHistoryController::class, 'create'])
+            ->middleware(['permission:can_create_employment_history']);
     });
 });
