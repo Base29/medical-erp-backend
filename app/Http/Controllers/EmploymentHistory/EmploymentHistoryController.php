45a127d8ab55f2d87aef5cefc68f1e30e9254b67
Faisal Hussain <faisal@base29.com>
2021-12-09 18:29:09 +0500

    created a method for updating employment history app/Http/Controllers/EmploymentHistory/EmploymentHistoryController.php

--- app/Http/Controllers/EmploymentHistory/EmploymentHistoryController.php
+++ app/Http/Controllers/EmploymentHistory/EmploymentHistoryController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\EmploymentHistory;
 
 use App\Helpers\Response;
+use App\Helpers\ResponseMessage;
+use App\Helpers\UpdateService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\EmploymentHistory\CreateEmploymentHistoryRequest;
+use App\Http\Requests\EmploymentHistory\UpdateEmploymentHistoryRequest;
 use App\Models\EmploymentHistory;
 use App\Models\User;
 
@@ -44,6 +47,63 @@ class EmploymentHistoryController extends Controller
 
         } catch (\Exception$e) {
 
+            return Response::fail([
+                'code' => 500,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    // Update employment history
+    public function update(UpdateEmploymentHistoryRequest $request)
+    {
+        try {
+
+            // Allowed fields
+            $allowedFields = [
+                'employer_name',
+                'address',
+                'phone_number',
+                'type_of_business',
+                'job_title',
+                'job_start_date',
+                'job_end_date',
+                'salary',
+                'reporting_to',
+                'period_of_notice',
+                'can_contact_referee',
+                'reason_for_leaving',
+                'responsibilities_duties_desc',
+                'is_current',
+            ];
+
+            // Checking if the $request doesn't contain any of the allowed fields
+            if (!$request->hasAny($allowedFields)) {
+                return Response::fail([
+                    'message' => ResponseMessage::allowedFields($allowedFields),
+                    'code' => 400,
+                ]);
+            }
+
+            // Get employment history by Id ($request->employment_history)
+            $employmentHistory = EmploymentHistory::findOrFail($request->employment_history);
+
+            // Update $employmentHistory
+            $employmentHistoryUpdated = UpdateService::updateModel($employmentHistory, $request->all(), 'employment_history');
+
+            if (!$employmentHistoryUpdated) {
+                return Response::fail([
+                    'code' => 400,
+                    'message' => ResponseMessage::customMessage('Something went wrong. Cannot update Employment History.'),
+                ]);
+            }
+
+            // Return success response
+            return Response::success([
+                'employment-history' => $employmentHistory->latest('updated_at')->first(),
+            ]);
+
+        } catch (\Exception$e) {
             return Response::fail([
                 'code' => 500,
                 'message' => $e->getMessage(),
