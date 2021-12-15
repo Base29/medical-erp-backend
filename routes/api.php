4a63b9d69d7cf7b9ed38abe4d89efe7930c838c5
Faisal Hussain <faisal@base29.com>
2021-12-15 14:04:25 +0500

    added route for creating references

--- routes/api.php
+++ routes/api.php
@@ -16,6 +16,7 @@ use App\Http\Controllers\Post\PostController;
 use App\Http\Controllers\Practice\PracticeController;
 use App\Http\Controllers\Profile\ProfileController;
 use App\Http\Controllers\Reason\ReasonController;
+use App\Http\Controllers\Reference\ReferenceController;
 use App\Http\Controllers\Role\RoleController;
 use App\Http\Controllers\Room\RoomController;
 use App\Http\Controllers\Signature\SignatureController;
@@ -354,5 +355,11 @@ Route::middleware(['auth:api'])->group(function () {
 
         Route::post('employment-history', [EmploymentHistoryController::class, 'fetchSingle'])
             ->middleware(['permission:can_fetch_single_employment_history']);
+    });
+
+    // Routes for references
+    Route::prefix('references')->group(function () {
+        Route::post('create', [ReferenceController::class, 'create'])
+            ->middleware(['permission:can_create_reference']);
     });
 });
