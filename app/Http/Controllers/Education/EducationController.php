d47142457fb337cb75a0070523a252a32a7763b1
Faisal Hussain <faisal@base29.com>
2021-12-17 16:23:29 +0500

    created functionality for creating user's education

---
+++ app/Http/Controllers/Education/EducationController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers\Education;
+
+use App\Helpers\FileUploadService;
+use App\Helpers\Response;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Education\CreateEducationRequest;
+use App\Models\Education;
+use App\Models\User;
+
+class EducationController extends Controller
+{
+    // Create Education
+    public function create(CreateEducationRequest $request)
+    {
+        try {
+            // Get user
+            $user = User::findOrFail($request->user);
+
+            // Initiate a null variable
+            $educationCertificateUrl = null;
+
+            // Check if request has certificate file to upload
+            if ($request->hasFile('certificate')) {
+                // Folder path for education certificate
+                $folderPath = 'education/user-' . $user->id;
+
+                // Education certificate upload
+                $educationCertificateUrl = FileUploadService::upload($request->certificate, $folderPath, 's3');
+            }
+
+            // Initiate a instance of Education model
+            $education = new Education();
+            $education->institution = $request->institution;
+            $education->subject = $request->subject;
+            $education->start_date = $request->start_date;
+            $education->end_date = $request->end_date;
+            $education->degree = $request->degree;
+            $education->grade = $request->grade;
+            $education->certificate = $educationCertificateUrl;
+
+            // Save Education
+            $user->education()->save($education);
+
+            // Return success response
+            return Response::success([
+                'education' => $education,
+            ]);
+
+        } catch (\Exception $e) {
+            return Response::fail([
+                'code' => 400,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+}
