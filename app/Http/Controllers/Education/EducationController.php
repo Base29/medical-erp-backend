6aaeae0945950c8e7bc333a5d0212654381623ff
Faisal Hussain <faisal@base29.com>
2021-12-20 16:58:12 +0500

    added functionality to uodate education

--- app/Http/Controllers/Education/EducationController.php
+++ app/Http/Controllers/Education/EducationController.php
@@ -5,10 +5,12 @@ namespace App\Http\Controllers\Education;
 use App\Helpers\FileUploadService;
 use App\Helpers\Response;
 use App\Helpers\ResponseMessage;
+use App\Helpers\UpdateService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Education\CreateEducationRequest;
 use App\Http\Requests\Education\DeleteEducationRequest;
 use App\Http\Requests\Education\FetchEducationRequest;
+use App\Http\Requests\Education\UpdateEducationRequest;
 use App\Models\Education;
 use App\Models\User;
 
@@ -97,6 +99,68 @@ class EducationController extends Controller
             // Return success response
             return Response::success([
                 'message' => ResponseMessage::deleteSuccess('Education'),
+            ]);
+
+        } catch (\Exception $e) {
+            return Response::fail([
+                'code' => 400,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    // Update education
+    public function update(UpdateEducationRequest $request)
+    {
+        try {
+
+            // Allowed fields
+            $allowedFields = [
+                'institution',
+                'subject',
+                'start_date',
+                'completion_date',
+                'degree',
+                'grade',
+                'certificate',
+            ];
+
+            // Checking if the $request doesn't contain any of the allowed fields
+            if (!$request->hasAny($allowedFields)) {
+                return Response::fail([
+                    'message' => ResponseMessage::allowedFields($allowedFields),
+                    'code' => 400,
+                ]);
+            }
+
+            // Get education
+            $education = Education::fidOrFail($request->education);
+
+            $updateRequestData = $request->all();
+
+            // Check if request has file $request->certificate
+            if ($request->hasFile('certificate')) {
+                // Folder path for education certificate
+                $folderPath = 'education/user-' . $education->user_id;
+
+                // Upload file
+                $updateRequestData['certificate'] = FileUploadService::upload($request->file('certificate'), $folderPath, 's3');
+
+            }
+
+            // Update education
+            $educationUpdated = UpdateService::updateModel($education, $updateRequestData, 'education');
+
+            if (!$educationUpdated) {
+                return Response::fail([
+                    'code' => 400,
+                    'message' => ResponseMessage::customMessage('Something went wrong. Cannot update Education at this moment'),
+                ]);
+            }
+
+            // Return success response
+            return Response::success([
+                'education' => $education->latest('updated_at')->first(),
             ]);
 
         } catch (\Exception $e) {
