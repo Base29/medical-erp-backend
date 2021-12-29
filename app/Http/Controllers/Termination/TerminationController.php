b7391aae32b0a3a0f9ad883b145c69c1883d6c2e
Faisal Hussain <faisal@base29.com>
2021-12-29 14:35:02 +0500

    update termination functionality

--- app/Http/Controllers/Termination/TerminationController.php
+++ app/Http/Controllers/Termination/TerminationController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Termination;
 
 use App\Helpers\Response;
+use App\Helpers\ResponseMessage;
+use App\Helpers\UpdateService;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Termination\CreateTerminationRequest;
 use App\Http\Requests\Termination\FetchTerminationRequest;
+use App\Http\Requests\Termination\UpdateTerminationRequest;
 use App\Models\Termination;
 use App\Models\User;
 
@@ -53,6 +56,53 @@ class TerminationController extends Controller
             // Return success response
             return Response::success([
                 'termination' => $termination,
+            ]);
+
+        } catch (\Exception $e) {
+            return Response::fail([
+                'code' => 400,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
+
+    // Update termination
+    public function update(UpdateTerminationRequest $request)
+    {
+        try {
+            // Allowed fields
+            $allowedFields = [
+                'date',
+                'reason',
+                'detail',
+                'is_exit_interview_performed',
+            ];
+
+            // Checking if the $request doesn't contain any of the allowed fields
+            if (!$request->hasAny($allowedFields)) {
+                return Response::fail([
+                    'message' => ResponseMessage::allowedFields($allowedFields),
+                    'code' => 400,
+                ]);
+            }
+
+            // Get termination
+            $termination = Termination::findOrFail($request->termination);
+
+            // Update termination
+            $terminationUpdated = UpdateService::updateModel($termination, $request->all(), 'termination');
+
+            // Return fail response in-case model is not updated
+            if (!$terminationUpdated) {
+                return Response::fail([
+                    'code' => 400,
+                    'message' => ResponseMessage::customMessage('Something went wrong. Cannot update Termination'),
+                ]);
+            }
+
+            // Return success response
+            return Response::success([
+                'termination' => $termination->latest('updated_at')->first(),
             ]);
 
         } catch (\Exception $e) {
