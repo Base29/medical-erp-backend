3e438596e4a65fc8feb054a1f4021501c1af2c90
Faisal Hussain <faisal@base29.com>
2021-12-30 19:01:45 +0500

    created app/Http/Requests/HiringRequest/CreateHiringRequest.php

---
+++ app/Http/Controllers/HiringRequest/HiringRequestController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\HiringRequest;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class HiringRequestController extends Controller
+{
+    //
+}
