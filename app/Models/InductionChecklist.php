518781a92f36ae854cc230b1e1579953d58b6a9f
Faisal Hussain <faisal@base29.com>
2022-01-05 18:09:24 +0500

    defined relation between InductionCheclist and InductionQuestion

--- app/Models/InductionChecklist.php
+++ app/Models/InductionChecklist.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Models\InductionQuestion;
 use App\Models\Practice;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
@@ -20,5 +21,10 @@ class InductionChecklist extends Model
     public function practice()
     {
         return $this->belongsTo(Practice::class);
+    }
+
+    public function inductionQuestions()
+    {
+        return $this->hasMany(InductionQuestion::class);
     }
 }
--- app/Models/InductionQuestion.php
+++ app/Models/InductionQuestion.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\InductionChecklist;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InductionQuestion extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'induction_checklist_id',
+        'question',
+    ];
+
+    public function inductionChecklist()
+    {
+        return $this->belongsTo(InductionChecklist::class);
+    }
 }
