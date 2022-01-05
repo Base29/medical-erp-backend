98f1a2a176ef675629e75d9f20e8f16ccc15dd09
Faisal Hussain <faisal@base29.com>
2022-01-05 18:37:32 +0500

    defined relation between InductionSchecdule and User

--- app/Models/InductionSchedule.php
+++ app/Models/InductionSchedule.php
@@ -2,10 +2,17 @@
 
 namespace App\Models;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class InductionSchedule extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    public function users()
+    {
+        return $this->hasMany(User::class);
+    }
 }
