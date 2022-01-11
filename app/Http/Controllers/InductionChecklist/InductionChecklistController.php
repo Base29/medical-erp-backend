11b808f0bd1cba1ae5d530607d99b8490e7c4012
Faisal Hussain <faisal@base29.com>
2022-01-11 19:59:11 +0500

    created controller and request layer for InductionChecklist

---
+++ app/Http/Controllers/InductionChecklist/InductionChecklistController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\InductionChecklist;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class InductionChecklistController extends Controller
+{
+    //
+}
---
+++ app/Http/Requests/InductionChecklist/CreateInductionChecklistRequest.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Requests\InductionChecklist;
+
+use App\Helpers\CustomValidationService;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class CreateInductionChecklistRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'practice_id' => 'required|numeric|exists:practices,id',
+            'role_id' => 'required|numeric|exists:roles,id',
+            'name' => 'required|string|max:255',
+        ];
+    }
+
+    public function failedValidation($validator)
+    {
+        throw new ValidationException($validator, CustomValidationService::error_messages($this->rules(), $validator));
+    }
+}
