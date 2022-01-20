40c1c7d03628aaf4f48a68901fbe1b49083151db
Faisal Hussain <faisal@base29.com>
2022-01-20 16:09:13 +0500

    create department

--- app/Http/Controllers/Department/DepartmentController.php
+++ app/Http/Controllers/Department/DepartmentController.php
@@ -2,10 +2,38 @@
 
 namespace App\Http\Controllers\Department;
 
+use App\Helpers\Response;
 use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
+use App\Http\Requests\Department\CreateDepartmentRequest;
+use App\Models\Department;
+use App\Models\Practice;
 
 class DepartmentController extends Controller
 {
-    //
+    // Create department
+    public function create(CreateDepartmentRequest $request)
+    {
+        try {
+            // Get practice
+            $practice = Practice::findOrFail($request->practice);
+
+            // Instance of Department model
+            $department = new Department();
+            $department->name = $request->name;
+
+            // Save department
+            $practice->departments()->save($department);
+
+            // Return success response
+            return Response::success([
+                'department' => $department->with('practice')->latest()->first(),
+            ]);
+
+        } catch (\Exception $e) {
+            return Response::fail([
+                'code' => 400,
+                'message' => $e->getMessage(),
+            ]);
+        }
+    }
 }
