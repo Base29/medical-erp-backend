20593a4d4d0b9da0a890920481cbe364fe99d115
Faisal Hussain <faisal@base29.com>
2022-03-22 13:09:28 +0500

    added migrations for locum_sessions

--- database/migrations/2022_03_21_095117_create_locum_sessions_table.php
+++ database/migrations/2022_03_21_095117_create_locum_sessions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class MakeStatusFieldNullableInOffersTable extends Migration
+class CreateLocumSessionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,8 +13,9 @@ class MakeStatusFieldNullableInOffersTable extends Migration
      */
     public function up()
     {
-        Schema::table('offers', function (Blueprint $table) {
-            $table->string('status')->nullable()->change();
+        Schema::create('locum_sessions', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
         });
     }
 
@@ -25,8 +26,6 @@ class MakeStatusFieldNullableInOffersTable extends Migration
      */
     public function down()
     {
-        Schema::table('offers', function (Blueprint $table) {
-            $table->string('status')->default('made')->nullable(false)->change();
-        });
+        Schema::dropIfExists('locum_sessions');
     }
 }
