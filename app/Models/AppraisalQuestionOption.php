092249efbde993c5b886ec474aa5ad735c900021
Faisal Hussain <faisal@base29.com>
2022-07-18 16:38:17 +0500

    added relations for Appraisal Question Option

--- app/Models/AppraisalQuestionOption.php
+++ app/Models/AppraisalQuestionOption.php
@@ -2,10 +2,22 @@
 
 namespace App\Models;
 
+use App\Models\AppraisalQuestion;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class AppraisalQuestionOption extends Model
 {
-    use HasFactory;
+    use HasFactory, SoftDeletes;
+
+    protected $fillable = [
+        'appraisal_question',
+        'option',
+    ];
+
+    public function appraisalQuestion()
+    {
+        return $this->belongsTo(AppraisalQuestion::class);
+    }
 }
