e0f704b7091114f9695d3bd9c67408f57cb2886b
Faisal Hussain <faisal@base29.com>
2022-07-27 17:09:40 +0500

    added request for create course module

---
+++ app/Http/Requests/TrainingCourse/CreateCourseModuleRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\TrainingCourse;
+
+use App\Helpers\CustomValidationService;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\ValidationException;
+
+class CreateCourseModuleRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    public function failedValidation($validator)
+    {
+        throw new ValidationException($validator, CustomValidationService::error_messages($this->rules(), $validator));
+    }
+}
