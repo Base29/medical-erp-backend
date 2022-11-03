8c39f559a50ffed686f34391f557420ee61abdd5
Faisal Hussain <faisal@base29.com>
2022-11-03 17:29:59 +0500

    fixed typo in table name of the migration

--- database/migrations/2022_11_03_115011_add_answer_column_to_candidate_answers_table.php
+++ database/migrations/2022_11_03_115011_add_answer_column_to_candidate_answers_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::table('candidate_answers', function (Blueprint $table) {
+        Schema::table('candidate_questions', function (Blueprint $table) {
             $table->text('answer')->nullable()->after('question');
         });
     }
@@ -25,7 +25,7 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::table('candidate_answers', function (Blueprint $table) {
+        Schema::table('candidate_questions', function (Blueprint $table) {
             $table->dropColumn('answer');
         });
     }
