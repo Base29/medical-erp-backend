df9ce6f768489ea1e6ccc650f4f7b5c7941d54d0
Faisal Hussain <faisal@base29.com>
2022-11-14 15:27:23 +0500

    modified migration. Modified request for creating policies

--- app/Http/Requests/Policy/CreatePolicyRequest.php
+++ app/Http/Requests/Policy/CreatePolicyRequest.php
@@ -4,6 +4,7 @@ namespace App\Http\Requests\Policy;
 
 use App\Helpers\CustomValidationService;
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
 use Illuminate\Validation\ValidationException;
 
 class CreatePolicyRequest extends FormRequest
@@ -26,7 +27,13 @@ class CreatePolicyRequest extends FormRequest
     public function rules()
     {
         return [
-            'name' => 'required|unique:policies,name',
+            'name' => 'nullable|unique:policies,name',
+            'description' => 'nullable|string|max:2000',
+            'type' => [
+                'nullable',
+                'string',
+                Rule::in(['clinical']),
+            ],
             'attachment' => 'required|file|mimes:doc,docx,pdf',
             'practice' => 'required|numeric|exists:practices,id',
         ];
