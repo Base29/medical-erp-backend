fb14f1189ec086ef990d1b0cc69e5b6e8f7b0a0c
Faisal Hussain <faisal@base29.com>
2022-11-18 20:43:39 +0500

    Refactored WorkTimingService

--- app/Services/WorkTiming/WorkTimingService.php
+++ app/Services/WorkTiming/WorkTimingService.php
@@ -5,6 +5,7 @@ use App\Helpers\Response;
 use App\Helpers\ResponseMessage;
 use App\Helpers\UpdateService;
 use App\Models\WorkTiming;
+use Exception;
 
 class WorkTimingService
 {
@@ -21,7 +22,7 @@ class WorkTimingService
 
         // Checking if the $request doesn't contain any of the allowed fields
         if (!$request->hasAny($allowedFields)) {
-            throw new \Exception(ResponseMessage::allowedFields($allowedFields));
+            throw new Exception(ResponseMessage::allowedFields($allowedFields), Response::HTTP_BAD_REQUEST);
         }
 
         // Get work timing
@@ -31,10 +32,11 @@ class WorkTimingService
         $workTimingUpdated = UpdateService::updateModel($workTiming, $request->validated(), 'work_timing');
 
         if (!$workTimingUpdated) {
-            throw new \Exception(ResponseMessage::customMessage('Something went wrong while updating Work Timing ' . $workTiming->id));
+            throw new Exception(ResponseMessage::customMessage('Something went wrong while updating Work Timing ' . $workTiming->id), Response::HTTP_BAD_REQUEST);
         }
 
         return Response::success([
+            'code' => Response::HTTP_OK,
             'work_timing' => $workTiming->with('workPattern')->latest('updated_at')->first(),
         ]);
     }
@@ -45,6 +47,7 @@ class WorkTimingService
         $workTimings = WorkTiming::where('work_pattern_id', $request->work_pattern)->with('workPattern')->latest()->get();
 
         return Response::success([
+            'code' => Response::HTTP_OK,
             'work_timings' => $workTimings,
         ]);
     }
