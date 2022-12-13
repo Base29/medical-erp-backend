dc6ac29d715cd9a02dc3b50a15749292fee4bed2
Faisal Hussain <faisal@base29.com>
2022-12-13 14:40:39 +0500

    created observer for TainingCourse

---
+++ app/Observers/TrainingCourse/TrainingCourseObserver.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Observers\TrainingCourse;
+
+use App\Models\TrainingCourse;
+
+class TrainingCourseObserver
+{
+    /**
+     * Handle the TrainingCourse "created" event.
+     *
+     * @param  \App\Models\TrainingCourse  $trainingCourse
+     * @return void
+     */
+    public function created(TrainingCourse $trainingCourse)
+    {
+        //
+    }
+
+    /**
+     * Handle the TrainingCourse "updated" event.
+     *
+     * @param  \App\Models\TrainingCourse  $trainingCourse
+     * @return void
+     */
+    public function updated(TrainingCourse $trainingCourse)
+    {
+        //
+    }
+
+    /**
+     * Handle the TrainingCourse "deleted" event.
+     *
+     * @param  \App\Models\TrainingCourse  $trainingCourse
+     * @return void
+     */
+    public function deleted(TrainingCourse $trainingCourse)
+    {
+        //
+    }
+
+    /**
+     * Handle the TrainingCourse "restored" event.
+     *
+     * @param  \App\Models\TrainingCourse  $trainingCourse
+     * @return void
+     */
+    public function restored(TrainingCourse $trainingCourse)
+    {
+        //
+    }
+
+    /**
+     * Handle the TrainingCourse "force deleted" event.
+     *
+     * @param  \App\Models\TrainingCourse  $trainingCourse
+     * @return void
+     */
+    public function forceDeleted(TrainingCourse $trainingCourse)
+    {
+        //
+    }
+}
