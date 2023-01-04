1bbc9c7ee1ef76c4acbebf915205714649ef4dda
Faisal Hussain <faisal@base29.com>
2023-01-04 15:23:46 +0500

    assigning permissions to roles in RoleSeeder class

--- database/seeders/RoleSeeder.php
+++ database/seeders/RoleSeeder.php
@@ -36,13 +36,91 @@ class RoleSeeder extends Seeder
             'manager',
             'recruiter',
             're',
+            'ma',
+        ];
+
+        // Permissions for HQ
+        $permissionsForHq = [
+            'can_manage_hiring_requests',
+            'can_manage_interview',
+            'can_manage_offers',
+        ];
+
+        // Permissions for recruiter
+        $permissionsForRecruiter = [
+            'can_manage_interview',
+            'can_manage_interview_policy',
+            'can_manage_locums',
+            'can_manage_employee_handbook',
+            'can_manage_it_policy',
+            'can_manage_candidate',
+            'can_manage_hiring_requests',
+            'can_manage_contract_summaries',
+            'can_manage_position_summaries',
+            'can_manage_work_patterns',
+            'can_manage_profiles',
+            'can_manage_misc_info',
+            'can_manage_employment_checks',
+            'can_manage_employment_policies',
+            'can_manage_employment_histories',
+            'can_manage_references',
+            'can_manage_education',
+            'can_manage_legal',
+            'can_manage_emergency_contacts',
+            'can_manage_terminations',
+            'can_manage_hiring_requests',
+            'can_manage_job_specifications',
+            'can_manage_person_specifications',
+            'can_manage_offers',
+        ];
+
+        // Permissions for manager
+        $permissionsForManager = [
+            'can_manage_appraisal',
+            'can_manage_appraisal_policy',
+            'can_manage_training_course',
+            'can_manage_employees',
+            'can_manage_practices',
+            'can_manage_policies',
+            'can_manage_rooms',
+            'can_manage_reasons',
+            'can_manage_checklists',
+            'can_manage_tasks',
+            'can_manage_posts',
+            'can_manage_answers',
+            'can_manage_comments',
+            'can_manage_signatures',
+            'can_manage_work_patterns',
+            'can_manage_hiring_requests',
+            'can_manage_inductions',
+            'can_manage_departments',
+            'can_manage_user_objective',
         ];
 
         foreach ($roles as $role) {
-            Role::create([
+            $createdRole = Role::create([
                 'name' => $role,
                 'guard_name' => 'api',
             ]);
+
+            /**
+             * Assign permissions to roles
+             */
+
+            // Assign permissions to hq, headquarter role
+            if ($createdRole === 'hq' || $createdRole === 'headquarter') {
+                $createdRole->givePermissionTo($permissionsForHq);
+            }
+
+            // Assign permissions to re, recruiter role
+            if ($createdRole === 'recruiter' || $createdRole === 're') {
+                $createdRole->givePermissionTo($permissionsForRecruiter);
+            }
+
+            // Assign permission to ma, manager role
+            if ($createdRole === 'manager' || $createdRole === 'ma') {
+                $createdRole->givePermissionTo($permissionsForManager);
+            }
         }
     }
 }
