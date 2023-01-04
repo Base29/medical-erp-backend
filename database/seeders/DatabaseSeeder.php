dbae4f37dc33d31385bdd02d50ba6056d97f3332
Faisal Hussain <faisal@base29.com>
2023-01-04 19:36:46 +0500

    typo fixed in a filename

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            PermissionSeeder::class,
+            RoleSeeder::class,
+            SuperAdminSeeder::class,
+            ServiceUserSeeder::class,
+        ]);
     }
 }
