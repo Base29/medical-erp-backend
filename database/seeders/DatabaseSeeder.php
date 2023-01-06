9ff42cfa50a84857bd31495d73466e8ee49a6f92
Faisal Hussain <faisal@base29.com>
2023-01-06 17:47:58 +0500

    JobSpecificationSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,7 @@ class DatabaseSeeder extends Seeder
             PracticeSeeder::class,
             ProfileSeeder::class,
             DepartmentSeeder::class,
+            JobSpecificationSeeder::class,
         ]);
     }
 }
